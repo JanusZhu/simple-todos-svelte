1256a2bdecb3808660117624c23ab4f339f88bfb
JanusZhu <janus.zhu@yale.edu>
2023-07-12 20:53:27 -0400

    Added filters

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -1,23 +1,41 @@
 <script>
   import Task from './Task.svelte';
+  let hideCompleted = false
+  const setHideCompleted = () => {
+    hideCompleted = !hideCompleted
+  }
+  let incompleteCount;
+  let pendingTitle = '';
+  let tasks = []
 
   import { TasksCollection } from '../api/TasksCollection';
 
   import TaskForm from './TaskForm.svelte';
 
-  $m: tasks = TasksCollection.find({}, {sort:{createdAt:-1}}).fetch()
+  $m: {
+    tasks = TasksCollection.find(hideCompleted ? {isChecked: {$ne: true}} : {}, {sort:{createdAt:-1}}).fetch()
+    incompleteCount = TasksCollection.find({isChecked: {$ne: true}}).count();
+    pendingTitle = `${
+      incompleteCount ? `(${incompleteCount})` : ''
+    }`;
+  }
 </script>
 <div class="app">
   <header>
       <div class="app-bar">
           <div class="app-header">
-              <h1>📝️ To Do List</h1>
+              <h1>📝️ To Do List {pendingTitle}</h1>
           </div>
       </div>
   </header>
 
   <div class="main">
       <TaskForm />
+      <div class="filter">
+        <button on:click={setHideCompleted}>
+          {hideCompleted ? 'Show All' : 'Hide Completed'}
+        </button>
+      </div>
 
       <ul class="tasks">
           {#each tasks as task (task._id)}
