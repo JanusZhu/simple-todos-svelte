a95fef53742bb64acaca75510af4c3ae4ab8dda5
Janus Zhu <janus.zhu@aya.yale.edu>
2023-09-14 09:42:52 +0000

    Thu, Sep 14, 2023, 5:42 PM +08:00

--- imports/ui/TaskForm.svelte
+++ imports/ui/TaskForm.svelte
@@ -1,19 +1,28 @@
 <script>
-    import {TasksCollection} from '../api/TasksCollection'
-    let newTask = ''
+    import { TasksCollection } from '../api/TasksCollection';
+
+    export let user = null;
+    let newTask = '';
+
     const handleSubmit = () => {
-        //insert the new task
-        TasksCollection.insert({
-            text:newTask,
-            createdAt: new Date(),
-        })
-        //clear the form
-        newTask = ''
+      // Insert a task into the collection
+      TasksCollection.insert({
+        text: newTask,
+        createdAt: new Date(), // current time
+        userId: user._id,
+      });
+
+      // Clear form
+      newTask = '';
     }
 </script>
 
 <form class="task-form" on:submit|preventDefault={handleSubmit}>
-    <input type="text" name="text" placeholder="Type to add new tasks" 
-    bind:value = {newTask}/>
+    <input
+            type="text"
+            name="text"
+            placeholder="Type to add new tasks"
+            bind:value={newTask}
+    />
     <button type="submit">Add Task</button>
 </form>
