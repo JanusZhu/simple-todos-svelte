83a2b727bf979d6389c4d721c91329975658df8c
Janus Zhu <janus.zhu@aya.yale.edu>
2023-09-14 10:11:36 +0000

    Thu, Sep 14, 2023, 6:11 PM +08:00

--- imports/api/tasksMethods.tests.js
+++ imports/api/tasksMethods.tests.js
@@ -1,6 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random';
+import { mockMethodCall } from 'meteor/quave:testing';
+import { assert } from 'chai';
 import { TasksCollection } from '/imports/db/TasksCollection';
+import '/imports/api/tasksMethods';
 
 if (Meteor.isServer) {
   describe('Tasks', () => {
@@ -15,6 +18,12 @@ if (Meteor.isServer) {
           createdAt: new Date(),
           userId,
         });
+      });
+
+      it('can delete owned task', () => {
+        mockMethodCall('tasks.remove', taskId, { context: { userId } });
+
+        assert.equal(TasksCollection.find().count(), 0);
       });
     });
   });
