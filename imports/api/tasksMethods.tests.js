1ee8ef09f23153bcba96d0e60458c9820caf1deb
Janus Zhu <janus.zhu@aya.yale.edu>
2023-09-14 10:12:35 +0000

    Thu, Sep 14, 2023, 6:12 PM +08:00

--- imports/api/tasksMethods.tests.js
+++ imports/api/tasksMethods.tests.js
@@ -25,6 +25,42 @@ if (Meteor.isServer) {
 
         assert.equal(TasksCollection.find().count(), 0);
       });
+
+      it(`can't delete task without an user authenticated`, () => {
+        const fn = () => mockMethodCall('tasks.remove', taskId);
+        assert.throw(fn, /Not authorized/);
+        assert.equal(TasksCollection.find().count(), 1);
+      });
+
+      it(`can't delete task from another owner`, () => {
+        const fn = () =>
+          mockMethodCall('tasks.remove', taskId, {
+            context: { userId: 'somebody-else-id' },
+          });
+        assert.throw(fn, /Access denied/);
+        assert.equal(TasksCollection.find().count(), 1);
+      });
+
+      it('can change the status of a task', () => {
+        const originalTask = TasksCollection.findOne(taskId);
+        mockMethodCall('tasks.setIsChecked', taskId, !originalTask.isChecked, {
+          context: { userId },
+        });
+
+        const updatedTask = TasksCollection.findOne(taskId);
+        assert.notEqual(updatedTask.isChecked, originalTask.isChecked);
+      });
+
+      it('can insert new tasks', () => {
+        const text = 'New Task';
+        mockMethodCall('tasks.insert', text, {
+          context: { userId },
+        });
+
+        const tasks = TasksCollection.find({}).fetch();
+        assert.equal(tasks.length, 2);
+        assert.isTrue(tasks.some(task => task.text === text));
+      });
     });
   });
 }
